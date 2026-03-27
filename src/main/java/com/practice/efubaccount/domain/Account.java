49810504dcc5113f3f5165f9a6b4841a45359371
KKANGCHONG <leee0414@naver.com>
2026-03-28 02:51:28 +0900

    ✨ feat: Account 도메인 기본 구조 및 상태 enum 정의

---
+++ src/main/java/com/practice/efubaccount/domain/Account.java
@@ -0,0 +1,20 @@
+package com.practice.efubaccount.domain;
+
+public class Account {
+
+    // 회원 이메일
+    private String email;
+
+    // 회원 비밀번호
+    private String password;
+
+    // 회원 닉네임
+    private String nickname;
+
+    // 회원 자기소개
+    private String bio;
+
+    // 회원 상태
+    private AccountStatus status;
+
+}
