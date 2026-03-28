dc307c65bb12482d23d5e5b5f20ad54a4ac5804b
KKANGCHONG <leee0414@naver.com>
2026-03-29 01:40:07 +0900

    💚chore: week2 실습 코드 주석 처리

--- src/main/java/com/practice/efubaccount/dto/ExampleDto.java
+++ src/main/java/com/practice/efubaccount/dto/ExampleDto.java
@@ -2,31 +2,31 @@ package com.practice.efubaccount.dto;
 
 public class ExampleDto {
 
-    private String name;
-    private Long id;
-    private String password;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+//    private String name;
+//    private Long id;
+//    private String password;
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
 }
