9b4bd583181f0ca6a2d6e7cee3f436e44ad809db
jettieb <bokjihee@naver.com>
2026-05-12 01:51:29 +0900

    feat: 댓글 관련 api 구현

---
+++ src/main/java/com/practice/efubaccount/comment/repository/CommentLikeRepository.java
@@ -0,0 +1,15 @@
+package com.practice.efubaccount.comment.repository;
+
+import com.practice.efubaccount.account.domain.Account;
+import com.practice.efubaccount.comment.domain.Comment;
+import com.practice.efubaccount.comment.domain.CommentLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> {
+
+    boolean existsByCommentAndAccount(Comment comment, Account account);
+
+    Optional<CommentLike> findByCommentAndAccount(Comment comment, Account account);
+}
